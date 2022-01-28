fd1ebc5f8d4e893dd0b928f32ade05f230d46ab0
romanze <romanze76@hotmail.com>
2022-01-28 14:45:00 +0900

    finish resource management

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -3,6 +3,10 @@ package jade;
 import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
+import util.AssetPool;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
 
 public class LevelEditorScene extends Scene {
 
@@ -11,7 +15,7 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
-        this.camera = new Camera(new Vector2f());
+        this.camera = new Camera(new Vector2f(-250, 0));
 
         int xOffset = 10;
         int yOffset = 10;
@@ -20,23 +24,34 @@ public class LevelEditorScene extends Scene {
         float totalHeight = (float)(300 - yOffset * 2);
         float sizeX = totalWidth / 100.0f;
         float sizeY = totalHeight / 100.0f;
+        float padding = 0;
 
         for (int x = 0; x < 100; x++) {
             for (int y = 0; y < 100; y++) {
-                float xPos = xOffset + (x * sizeX);
-                float yPos = yOffset + (y * sizeY);
+                float xPos = xOffset + (x * sizeX) + (padding * x);
+                float yPos = yOffset + (y * sizeY) + (padding * y);
 
                 GameObject go = new GameObject("obj" + x + "" + y, new Transform(new Vector2f(xPos, yPos), new Vector2f(sizeX, sizeY)));
                 go.addComponent(new SpriteRenderer(new Vector4f(xPos / totalWidth, yPos / totalHeight,1 , 1)));
                 this.addGameObjectToScene(go);
             }
         }
+
+        loadResources();
+    }
+
+    private void loadResources() {
+        AssetPool.getShader("assets/shaders/default.glsl");
     }
 
     @Override
     public void update(float dt) {
-        System.out.println("FPS: " + (1.0f / dt));
-
+        if (KeyListener.isKeyPressed(GLFW_KEY_UP)) {
+            camera.position.y += 100f * dt;
+        }
+        else if (KeyListener.isKeyPressed(GLFW_KEY_DOWN)) {
+            camera.position.y -= 100f * dt;
+        }
         for (GameObject go : this.gameObjects) {
             go.update(dt);
         }
